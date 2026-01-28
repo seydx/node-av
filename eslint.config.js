f6b99e67d7d9a16d13f26dd4ea30f6c70bdbbf19
seydx <dev@seydx.com>
2026-01-29 00:04:09 +0100

    Update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -21,6 +21,7 @@ export default [
       '**/.tmp/**',
       '**/externals/**',
       '**/examples/tests/**',
+      '**/examples/electron/**',
       '**/projects/**',
       '**/testdata/**',
       '**/ffbuild/**',
