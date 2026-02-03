15c6afaa7fb5162d3d5890a18d00ee1bdbb6010b
seydx <dev@seydx.com>
2026-02-03 17:00:27 +0100

    Remove test example

--- examples/test.ts
+++
@@ -1,7 +0,0 @@
-import { DeviceAPI } from '../src/index.js';
-
-console.log('Listing available devices...');
-
-const devices = await DeviceAPI.list();
-
-console.log('Available devices:', devices);
